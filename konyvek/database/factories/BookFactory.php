cf2fbea913df3174c6ce692a5e95ac0ecc8856c0
szludora <szlucska.dora@gmail.com>
2023-10-18 20:16:57 +0200

    factory kész, adatok

---
+++ konyvek/database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'author' => fake('hu_Hu')-> name(),
+            'title' => fake('hu_Hu')-> sentence(),
+            'pieces' => rand(60, 400),
+        ];
+    }
+}
--- konyvek/database/seeders/DatabaseSeeder.php
+++ konyvek/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Book;
+use App\Models\User;
+use App\Models\Copy;
+
+
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,7 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(20)->create();
+        Book::factory(20)->create();
+        Copy::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
